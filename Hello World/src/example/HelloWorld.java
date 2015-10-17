8cf255667a7f6d6c4594656e563399e09d553e3b
gemcfadyen <gemcfadyen@hotmail.co.uk>
2015-10-17 16:15:32 +0100

    Adds initial hello world

---
+++ Hello World/src/example/HelloWorld.java
@@ -0,0 +1,15 @@
+package example;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hallo Welt");
+		System.out.println("1/2 = " + 1 / 2); 
+		System.out.println("1/2.0 = " + 1 / 2.0); 
+		System.out.println("1.0/2 = " + 1.0 / 2); 
+		System.out.println("1.0/2.0 = " + 1.0 / 2.0);
+		System.out.println((int)-1); 
+	}
+
+}
