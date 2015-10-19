345833726808264ce909c473d9278d0c70ee5eeb
gemcfadyen <gemcfadyen@hotmail.co.uk>
2015-10-19 09:39:40 +0100

    Factors of 2 identified

--- Exercises/src/primes/PrimesTest.java
+++ Exercises/src/primes/PrimesTest.java
@@ -1,5 +1,21 @@
 package primes;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+
 public class PrimesTest {
 
+	@Test
+	public void factorsOf2() {
+		Assert.assertEquals(Arrays.asList(), factorsOf(2));
+	}
+
+	private List<Integer> factorsOf(int number) {
+		// TODO Auto-generated method stub
+		return Arrays.asList();
+	}
 }
