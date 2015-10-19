c5a31ab04d3799c7ef7194b658ab138d1cde2306
gemcfadyen <gemcfadyen@hotmail.co.uk>
2015-10-19 18:13:30 +0100

    Implements Bowling Game kata

--- Exercises/src/bowling/BowlingGame.java
+++ Exercises/src/bowling/BowlingGame.java
@@ -1,13 +1,50 @@
 package bowling;
 
 public class BowlingGame {
-	private int score = 0;
+	private int[] pins = new int[22];
+	private int rollIndex = 0;
 
-	public void roll(int numberOfPinsKnockedDown) {
-		score += numberOfPinsKnockedDown;
+	public void roll(int pin) {
+		pins[rollIndex] = pin;
+		rollIndex++;
 	}
 
-	public int getScore() {
+	public int score() {
+		int score = 0;
+		int roll = 0;
+		for (int frame = 0; frame < 11; frame++) {
+			if (isSpare(roll)) {
+				score += 10 + spareBonus(roll);
+				roll += 2;
+			} else if (isStrike(roll)) {
+				score += 10 + strikeBonus(roll);
+				roll += 1;
+			} else {
+				score += regularScore(roll);
+				roll += 2;
+			}
+
+		}
 		return score;
+	}
+
+	private int strikeBonus(int roll) {
+		return pins[roll + 1] + pins[roll + 2];
+	}
+
+	private boolean isStrike(int roll) {
+		return pins[roll] == 10;
+	}
+
+	private int regularScore(int roll) {
+		return pins[roll] + pins[roll + 1];
+	}
+
+	private int spareBonus(int roll) {
+		return pins[roll + 2];
+	}
+
+	private boolean isSpare(int roll) {
+		return regularScore(roll) == 10;
 	}
 }
--- Exercises/src/bowling/BowlingGameTest.java
+++ Exercises/src/bowling/BowlingGameTest.java
@@ -1,28 +1,61 @@
 package bowling;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
-	@Test
-	public void scoreIsZeroWhenPlayerKnocksZeroPinsPerFrame() {
-		BowlingGame game = new BowlingGame();
 
-		for (int rolls = 0; rolls < 20; rolls++) {
-			game.roll(0);
-		}
+	private BowlingGame game;
 
-		Assert.assertEquals(game.getScore(), 0);
+	@Before
+	public void setup() {
+		game = new BowlingGame();
 	}
 
 	@Test
-	public void scoreIsTwentyWhenPlayerKnocksOnePinPerFrame() {
-		BowlingGame game = new BowlingGame();
+	public void gutterGame() {
+		rollMultipleTimes(0, 20);
 
-		for (int rolls = 0; rolls < 20; rolls++) {
-			game.roll(1);
-		}
-
-		Assert.assertEquals(game.getScore(), 20);
+		Assert.assertEquals(0, game.score());
 	}
+
+	@Test
+	public void onePinPerRoll() {
+		rollMultipleTimes(1, 20);
+
+		Assert.assertEquals(20, game.score());
+	}
+
+	@Test
+	public void spare() {
+		game.roll(5);
+		game.roll(5);
+		game.roll(3);
+
+		Assert.assertEquals(16, game.score());
+	}
+
+	@Test
+	public void strike() {
+		game.roll(10);
+		game.roll(5);
+		game.roll(3);
+
+		Assert.assertEquals(26, game.score());
+	}
+
+	@Test
+	public void perfect() {
+		rollMultipleTimes(10, 11);
+		Assert.assertEquals(300, game.score());
+
+	}
+
+	private void rollMultipleTimes(int pins, int numberOfTurns) {
+		for (int turns = 0; turns < numberOfTurns; turns++) {
+			game.roll(pins);
+		}
+	}
+
 }
